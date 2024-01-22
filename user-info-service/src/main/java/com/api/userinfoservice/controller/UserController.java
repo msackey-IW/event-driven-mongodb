78e8240983bbd97a7955acb9dfcf4cddc0aeba66
msackey-IW <mathias.sackey@integration.works>
2024-01-22 15:23:22 +1300

    added api count metric

--- user-info-service/src/main/java/com/api/userinfoservice/controller/UserController.java
+++ user-info-service/src/main/java/com/api/userinfoservice/controller/UserController.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.api.userinfoservice.service.UserServiceImpl;
 
-import lombok.AllArgsConstructor;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.HttpStatus;
@@ -15,14 +16,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.api.userinfoservice.exceptions.InvalidArgumentException;
 import com.api.userinfoservice.models.*;
 
-@AllArgsConstructor
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
     private final UserServiceImpl userService;
+    private final Counter apiCallCounter;
 
+
+    public UserController(UserServiceImpl userService, MeterRegistry meterRegistry ) {
+        this.userService = userService;
+        this.apiCallCounter = Counter.builder("api.calls")
+                .description("Total number of API calls")
+                .register(meterRegistry);
+
+    }
     @GetMapping("/{userId}")
     public ResponseEntity<User> getUser(@PathVariable String userId) {
+        apiCallCounter.increment();
         Long id;
         try {
             id = Long.valueOf(userId);
