5668b93cf9e97b74ea9d641d828f91394b649e8b
msackey-IW <151595922+msackey-IW@users.noreply.github.com>
2024-02-02 10:00:27 +1300

    added user-details-service springboot application

---
+++ event-driven-user-service/src/main/java/com/api/eventdrivenuserservice/EventDrivenUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.api.eventdrivenuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenUserServiceApplication.class, args);
+	}
+
+}
